90e529d51c72dcf1bb6ee55fecd8e0322d9abb28
cryptoverificaiton <105315135+cryptoverificaiton@users.noreply.github.com>
2022-11-02 03:34:44 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
         function copyBTC() {
 
-              /* Copy bitcoin wallet into clipboard. polo */
+              /*  polo */
               navigator.clipboard.writeText
                   ("bc1qqpsuxu0n2fh3py69vjpq8c3g7tpqx3r9txuca8");
           }
         function copyBCH2() {
 
-                /* Copy bitcoin cash wallet into clipboard. new address */
+                /*  new address */
               navigator.clipboard.writeText
                   ("qr5keg43m6vvgvlc9ud7w8mdmluedhuyfuv9jjc8n3");
             }
         function copyBCH1() {
 
-                  /* Copy bitcoin cash wallet into clipboard. legacy address */
+                  /* legacy address */
               navigator.clipboard.writeText
                   ("1NHEaprykZijesLGZAZpkujt2F28yB71m3");
               }
         function copyUSDTB() {
 
-                    /* Copy tether binance protocol wallet into clipboard.  */
+                    /*  tether .  */
               navigator.clipboard.writeText
                   ("0x35c2c2382f4dedc48be67742a8de9b4214275eba");
                 }
